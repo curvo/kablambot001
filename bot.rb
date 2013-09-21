72408f19d8812d0d22a7b10dfaf4445e754a0576
Brian Case <brian@tobi.com>
2013-09-21 10:59:18 -0700

    reduced strategy to almost nothing

--- bot.rb
+++ bot.rb
@@ -33,13 +33,14 @@ module Bot
   
   def go!
     enemy = opponents.first
-    return fire_at!(enemy, 0.75) if can_fire_at?(enemy) && my.ammo > 0
-    return dodge(enemy) if enemy.can_fire_at?(me)   
-    return rest if my.ammo == 0
-    return aim_at!(enemy) unless aiming_at?(enemy)
-    return move_towards!(enemy) if obscured?(enemy)
-    # return rest if my.ammo < 10
-    return hunt unless enemy
+    return fire_at!(enemy, line_of_site_to(enemy)) if can_fire_at?(enemy)
+     # && my.ammo > 0
+    # return dodge(enemy) if enemy.can_fire_at?(me)   
+    # return rest if my.ammo == 0
+    # return aim_at!(enemy) unless aiming_at?(enemy)
+    # return move_towards!(enemy) if obscured?(enemy)
+    # # return rest if my.ammo_full?
+    # return hunt unless enemy            
     rest
   end
 end
